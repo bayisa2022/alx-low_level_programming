3a16d855a951269bd926edfbea9782284af31ef7
bayisa2022 <bayisa2022@gmail.com>
2022-09-08 13:40:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#includei<stdio.h>
 /*
  *explain Prints out print out string-Programming is like building a multilingual puzzle
  *
@@ -7,7 +7,7 @@
  */
  int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
