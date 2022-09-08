d7aa1321a1058bfd151466aeb2770f5c2d98619b
bayisa2022 <bayisa2022@gmail.com>
2022-09-08 14:43:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put funtion
  *
  * Description:using the min function
  * with proper grammar, but the outcome is a piece of art,
  * return: 0
- */
+ */ 
  int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-	return(0)
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
+}
