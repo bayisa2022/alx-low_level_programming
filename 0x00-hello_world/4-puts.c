3f7612e1538f45f947afc09b5de295833350cc17
bayisa2022 <bayisa2022@gmail.com>
2022-09-08 22:24:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include <stdio.>
+
 
 /**
  *
- * Description: main - Prints out a string to stdout.
+ * main - Entry point
  *
- * Return: 0 if success.
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
 
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
