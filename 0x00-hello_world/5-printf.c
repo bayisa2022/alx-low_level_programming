b2766983c2a0205c749dd739aefd0e389e5de57b
bayisa2022 <bayisa2022@gmail.com>
2022-09-09 04:47:45 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * main - Prints a text
  * Description: using the main function  
- * Return:o  
+ * Return:0  
  */
 int main(void)
 
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
