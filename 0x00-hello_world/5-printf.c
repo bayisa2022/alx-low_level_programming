6f7f4de0aa4750e98fd8c0cae592daf6fa02bfd8
bayisa2022 <bayisa2022@gmail.com>
2022-09-09 05:18:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text
- *
- * Description: using the main function 
- * Return:0
- */
+* main - Prints a text
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of
+art,\n");
+return (0);
 }
