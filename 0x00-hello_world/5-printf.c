fb4b9aa5fb6bd93a37fded7e079ac7ee38c348f0
bayisa2022 <bayisa2022@gmail.com>
2022-09-09 05:07:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Prints a text
+ *
  * Description: using the main function  
  *
- * Return:0  
+ * Return:0
  */
-int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
