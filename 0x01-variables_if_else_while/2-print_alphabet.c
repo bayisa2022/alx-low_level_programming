f95d7d1c14abe8e0a600ccc6e25c27200d9f87c8
bayisa2022 <bayisa2022@gmail.com>
2022-09-11 05:22:00 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,18 @@
 
 /**
  * main -main fuction 
+ *
  * Description:program that prints the alphabet in lowercase, followed by a new line
  * You can only use the putchar
  * Return: 0
  */
 int main(void)
 {
-	char c;
-	for (c = "a";c <= 'z';c++)
-		putchar(c);
-	putchar("\n");
-	return (0);
+char c;
+for (c = 'a';c <= 'z';c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
