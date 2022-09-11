c440a9d7a7d25634c4b716982c8457bf3790429c
bayisa2022 <bayisa2022@gmail.com>
2022-09-10 23:59:20 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-printf("\n");
-return (0);
+	int n;
+
+	for (n='n';n <= '9';n++)
+	{
+		putchar(n);
+	}
+	putchar("\n")
+	return 0;
 }
 
