8dab2b3d023878e1b0fbfac2c48073342a034b4b
bayisa2022 <bayisa2022@gmail.com>
2022-09-13 07:00:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main -Write a program that prints _putchar
@@ -16,6 +16,6 @@ int main (void)
         _putchar('a');                                                                                                                 
         _putchar('r');                                                                                                                  
         _putchar('\n');
-return 0;
+	return 0;
 }
 
