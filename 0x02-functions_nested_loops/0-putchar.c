1a006117224b4ffe6e4d476333dafa8abf9f731e
bayisa2022 <bayisa2022@gmail.com>
2022-09-13 07:27:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,17 @@
  *
  * return 0:always
  */
+
 int main (void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');  
-        _putchar('t');                                                                                                                  
-        _putchar('c');                                                                                                              
-        _putchar('h');
-        _putchar('a');                                                                                                                 
-        _putchar('r');                                                                                                                  
-        _putchar('\n');
-	return 0;
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");  
+        _putchar("t");                                                      
+        _putchar("c");                                                                    _putchar("h")
+	_putchar("a");                                                                    _putchar("r");                                                  
+	_putchar("\n");
+
+	return (0);
 }
 
