ae7d702725721e2d31bb5335e9b4d5b7511f94cf
bayisa2022 <bayisa2022@gmail.com>
2022-10-04 07:33:51 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
@@ -9,6 +10,7 @@
  *
  * Return: Nothing.
  */
+
 char *create_array(unsigned int size, char c)
 {
         unsigned int i;
