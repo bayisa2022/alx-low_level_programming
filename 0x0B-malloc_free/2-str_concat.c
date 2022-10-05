72d599875b61118d70c142cedb2ac82b48395cbc
bayisa2022 <bayisa2022@gmail.com>
2022-10-05 08:47:34 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid -  Entry point
+ *@width: int
+ *@height: int
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, l;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; *(s1 + i) != '\0'; i++)
+	{}
+	for (j = 0 ; *(s2 + j) != '\0' ; j++)
+	{}
+	p = malloc(sizeof(char) * (i + j + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		*(p + k) = *(s1 + k);
+	}
+	for (l = 0; l < j; l++)
+	{
+		*(p + (i + l)) = *(s2 + l);
+	}
+	p[i + j] = '\0';
+	return (p);
+
+}
