b2c7d0f5f88ebf122b8ad06a218c8cba46f1e6f3
bayisa2022 <bayisa2022@gmail.com>
2022-10-05 08:59:57 -0700

    task0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#inclue <stdlib.h>
+#include <stdlib.h>
 
-int _putchar(char)
+int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -12,4 +12,4 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
